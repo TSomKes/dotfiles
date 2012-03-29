e271e29ef9b05f0beeeef914862153732bc844c7
TSomKes <tsomkes@gmail.com>
2012-03-28 22:52:58 -0500

    Added empty .Xresources

--- create_symlinks.sh
+++ create_symlinks.sh
@@ -6,5 +6,6 @@ ln -s ~/code/dotfiles/hgrc ~/.hgrc
 ln -s ~/code/dotfiles/gvimrc ~/.gvimrc
 ln -s ~/code/dotfiles/vimrc ~/.vimrc
 ln -s ~/code/dotfiles/xinitrc ~/.xinitrc
+ln -s ~/code/dotfiles/Xresources ~/.Xresources
 
 
