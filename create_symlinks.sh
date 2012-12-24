5af0d9255ed20ac5a22c54e642fa71661e8ec834
TSomKes <tsomkes@gmail.com>
2012-12-24 00:12:12 -0600

    Added ability to force symlink creation
    This was because I often do something to mess up my symlinks:  rename
    the target folder, etc.--and removing them by hand was too much hassle.
    But I feared just adding a -f to the ln call, in case that ever went
    wrong.
    
    So this kinda got away from me, and turned into a time to play with
    getopts, etc.:  http://wiki.bash-hackers.org/howto/getopts_tutorial

--- create_symlinks.sh
+++ create_symlinks.sh
@@ -1,12 +1,45 @@
 #!/bin/bash
+#
+# I seem to reinstall whatever Linux distro I'm playing with distressingly
+# often.  This script just helps with wiring up the dotfiles that live on
+# github, so that I can get back to work.
 
-ln -s ~/dev/dotfiles/bashrc ~/.bashrc
-ln -s ~/dev/dotfiles/gitconfig ~/.gitconfig
-ln -s ~/dev/dotfiles/gitignore ~/.gitignore
-ln -s ~/dev/dotfiles/hgignore ~/.hgignore
-ln -s ~/dev/dotfiles/hgrc ~/.hgrc
-ln -s ~/dev/dotfiles/gvimrc ~/.gvimrc
-ln -s ~/dev/dotfiles/vimrc ~/.vimrc
-ln -s ~/dev/dotfiles/vim ~/.vim
-ln -s ~/dev/dotfiles/xinitrc ~/.xinitrc
-ln -s ~/dev/dotfiles/Xresources ~/.Xresources
+# Find the absolute path of this script
+# "Temporarily" cd to this script's location & pwd it into the variable
+DOTFILES_ABS_PATH=$(cd `dirname $0` && pwd)
+
+# Look for options listed in given string
+# ("fi" means we'll accept -f or -i, etc.)
+while getopts ":fhi" opt; do
+	case $opt in
+		# -f == Force the 
+		f)
+			FLAG="--force"
+		;;
+		h)
+			echo "  -f 		forces replacement of existing files"
+			echo "  -i 		prompt whether to replace existing files"
+			echo "  -h 		give this help list"
+			exit 1
+		;;
+		i)
+			FLAG="--interactive"
+		;;
+		\?)
+			echo "Invalid option: -$OPTARG" >&2
+			echo "Try -h for more information." >&2
+			exit 1
+		;;
+	esac
+done
+
+ln $FLAG -s $DOTFILES_ABS_PATH/bashrc ~/.bashrc
+ln $FLAG -s $DOTFILES_ABS_PATH/gitconfig ~/.gitconfig
+ln $FLAG -s $DOTFILES_ABS_PATH/gitignore ~/.gitignore
+ln $FLAG -s $DOTFILES_ABS_PATH/hgignore ~/.hgignore
+ln $FLAG -s $DOTFILES_ABS_PATH/hgrc ~/.hgrc
+ln $FLAG -s $DOTFILES_ABS_PATH/gvimrc ~/.gvimrc
+ln $FLAG -s $DOTFILES_ABS_PATH/vimrc ~/.vimrc
+ln $FLAG -s $DOTFILES_ABS_PATH/vim ~/.vim
+ln $FLAG -s $DOTFILES_ABS_PATH/xinitrc ~/.xinitrc
+ln $FLAG -s $DOTFILES_ABS_PATH/Xresources ~/.Xresources
