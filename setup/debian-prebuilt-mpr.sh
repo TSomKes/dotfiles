bb2b0a99b502c5830e1483e65c48ca80e4511d47
TSomKes <tsomkes@gmail.com>
2024-03-07 22:37:50 -0600

    Add prebuilt-MPR setup script

---
+++ setup/debian-prebuilt-mpr.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+# Originally done to install https://github.com/casey/just
+# via instructions here:  
+# - https://docs.makedeb.org/prebuilt-mpr/getting-started/
+
+wget -qO - 'https://proget.makedeb.org/debian-feeds/prebuilt-mpr.pub' | gpg --dearmor | sudo tee /usr/share/keyrings/prebuilt-mpr-archive-keyring.gpg 1> /dev/null
+echo "deb [arch=all,$(dpkg --print-architecture) signed-by=/usr/share/keyrings/prebuilt-mpr-archive-keyring.gpg] https://proget.makedeb.org prebuilt-mpr $(lsb_release -cs)" | sudo tee /etc/apt/sources.list.d/prebuilt-mpr.list
+sudo apt update
