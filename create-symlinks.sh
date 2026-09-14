be77166de0a1d912e8953903684f867fe00a9d29
TSomKes <tsomkes@gmail.com>
2026-09-14 11:34:36 -0500

    Remove spectrwm dotfile references

--- create-symlinks.sh
+++ create-symlinks.sh
@@ -41,8 +41,6 @@ ln $FLAG -s $DOTFILES_ABS_PATH/gitconfig ~/.gitconfig
 ln $FLAG -s $DOTFILES_ABS_PATH/gitignore ~/.gitignore
 ln $FLAG -s $DOTFILES_ABS_PATH/gvimrc ~/.gvimrc
 ln $FLAG -s $DOTFILES_ABS_PATH/profile ~/.profile
-# ln $FLAG -s $DOTFILES_ABS_PATH/spectrwm.conf ~/.spectrwm.conf
-# ln $FLAG -s $DOTFILES_ABS_PATH/spectrwm_us.conf ~/.spectrwm_us.conf
 ln $FLAG -s $DOTFILES_ABS_PATH/sqliterc ~/.sqliterc
 ln $FLAG -s $DOTFILES_ABS_PATH/ssh/config ~/.ssh/config
 ln $FLAG -s $DOTFILES_ABS_PATH/tmux.conf ~/.tmux.conf
